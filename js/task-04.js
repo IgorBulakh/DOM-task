ca39237bb509b6833f12df02f8dc5961081a7f20
IgorBulakh <bulach.igor@gmail.com>
2021-08-15 15:54:02 +0300

    four-task

---
+++ js/task-04.js
@@ -0,0 +1,35 @@
+/*
+Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+*/
+
+let valueNumberRef = Number(document.querySelector("#value").textContent);
+let counterValue = document.querySelector("#value");
+
+const decrementRef = document.querySelector('button[data-action="decrement"]');
+const incrementRef = document.querySelector('button[data-action="increment"]');
+
+// const onValueRef = () => {
+//   console.log(counterValue);
+// };
+
+// decrementRef.addEventListener("click", onValueRef);
+// incrementRef.addEventListener("click", onValueRef);
+
+decrementRef.addEventListener("click", (even) => {
+  valueNumberRef -= 1;
+
+  counterValue.textContent = valueNumberRef;
+  // counterValue.textContent = even.target.value--;
+  console.log(valueNumberRef);
+});
+incrementRef.addEventListener("click", (even) => {
+  valueNumberRef += 1;
+  counterValue.textContent = valueNumberRef;
+  // counterValue.textContent = even.target.value++;
+
+  console.log(valueNumberRef);
+});
--- js/task-test.js
+++ js/task-test.js
@@ -1,4 +1,45 @@
+/*
 console.log(document.body.firstElementChild);
 console.log(document.body.lastElementChild);
 console.log(document.body.children[1]);
 console.log(document.body.children[1].children[1]);
+*/
+//--------------------------------------------
+
+/*
+const user = {
+  name: "Mango",
+  showName() {
+    console.log(this);
+    // this будет ссылаться на button если использовать showName как callback
+    console.log(`My name is: ${this.name}`);
+    // тут undefined так как поля name у button нет
+  },
+};
+
+
+//  Представим что у нас есть кнопка с классом js-btn
+//  Выберем ее и повесим на нее слушатель клика
+ 
+const btn = document.querySelector(".js-btn");
+
+user.showName(); //работает
+btn.addEventListener("click", user.showName); // не работает
+btn.addEventListener("click", user.showName.bind(user)); // работает
+*/
+
+const addBtn = document.querySelector('button[data-action="add"]');
+const removeBtn = document.querySelector('button[data-action="remove"]');
+const btn = document.querySelector("#btn");
+
+const handleClick = () => {
+  alert("CLICK EVENT LISTENER ALERT!");
+};
+
+addBtn.addEventListener("click", () => {
+  btn.addEventListener("click", handleClick);
+});
+
+removeBtn.addEventListener("click", () => {
+  btn.removeEventListener("click", handleClick);
+});
