0cb48187b7f7c2a4bb56396c2af3a1385f2447b8
IgorBulakh <bulach.igor@gmail.com>
2021-08-30 17:06:40 +0300

    task_7-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,26 @@
 /*
 Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 */
+
+const inputRef = document.querySelector("#name-input");
+
+let spanRef = document.querySelector("#name-output");
+
+inputRef.addEventListener("input", (event) => {
+  const inputElem = event.target.value;
+
+  // inputRef !== " " ? (spanRef.textContent = inputElem) : "незнакомец";
+  inputRef === " "
+    ? (spanRef.textContent = "незнакомец")
+    : (spanRef.textContent = inputElem);
+});
+
+/*
+inputRef.onchange = function () {
+  if (inputRef.value !== "") {
+    spanRef.textContent = inputRef.value;
+  } else {
+    spanRef.textContent = "незнакомец";
+  }
+};
+*/
